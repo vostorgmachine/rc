87965fcefe0a4e94120ce6a59fb65d7cb85ac3e1
vostorg <106368478+vostorgmachine@users.noreply.github.com>
2022-10-19 16:20:52 +0000

    Delete countdown.sh

--- countdown.sh
+++
@@ -1,6 +0,0 @@
-seconds=2700
-start="$(($(date +%s) + $seconds))"
-while [ "$start" -ge `date +%s` ]; do
-	    time="$(( $start - `date +%s` ))"
-	        printf '%s\r' "$(date -u -d "@$time" +%H:%M:%S)"
-	done
